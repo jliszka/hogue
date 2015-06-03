788cf61ab5c1a5c520b2a0d38ccfed01b361489a
Jason Liszka <jliszka@alumni.cmu.edu>
2015-06-02 22:20:37 -0400

    request queries

--- src/Models/Request.hs
+++ src/Models/Request.hs
@@ -3,45 +3,39 @@
 
 module Models.Request where
 
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Maybe
 import GHC.Generics
 import Data.Aeson (ToJSON)
 import Data.Time.Clock (UTCTime, getCurrentTime)
 import Models.Query
+import qualified Models.User as User
+import Data.Bson (ObjectId)
 
-{-
 data Request = Request {
-  _id :: Field Request Int,
-  user_id :: Field Request String
-} deriving Generic
+  _id         :: Field Request ObjectId,
+  customer_id :: Field Request ObjectId,
+  desc        :: Field Request String
+} deriving (Show, Generic, ToJSON)
 
 instance Schema Request where
   schema = Request {
-    _id = field "_id",
-    user_id = field "user_id"
+    _id         = field "_id",
+    customer_id = field "customer_id",
+    desc        = field "desc"
   }
 
 instance Queryable Request where
   collection _ = "requests"
 
 
-q :: Query Request Request
-q = find [ _id $> 3, user_id $= "hi" ] $. asc _id $. limit 10
+-- q :: Query Request Request
+-- q = find [ _id $> 3, user_id $= "hi" ] $. orderAsc _id $. limit 10
 
-r :: Request
-r = Request {
-  _id = val 1,
-  user_id = val "hi"
-}
-
-i :: Int
-i = r ~. _id
-
-{-
-data Request = Request {
-  _id :: String,
-  user_id :: String,
-  created_at :: UTCTime
-} deriving (Show, Eq, Generic, ToJSON)
--}
-
--}
+go :: MaybeT IO [Request]
+go = do
+  db <- lift $ getDB "v2-staging"
+  let oid = read "5543eae3cfaa950f00dffbd6"
+  user <- MaybeT $ find [ User._id `eqs` oid ] $. fetchOne db
+  let uid = user ~. User._id
+  lift $ find [ customer_id `eqs` uid ] $. fetch db
